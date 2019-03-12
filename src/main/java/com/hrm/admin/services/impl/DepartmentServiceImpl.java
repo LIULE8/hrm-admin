bdf277f6461ea58abb7e6b1649f88977846e89fb
Jeffery Lu <jeffery.lu@oocl.com>
2019-03-12 19:40:42 +0800

    remove some exploring code

--- src/main/java/com/hrm/admin/services/impl/DepartmentServiceImpl.java
+++ src/main/java/com/hrm/admin/services/impl/DepartmentServiceImpl.java
@@ -87,33 +87,4 @@ public class DepartmentServiceImpl implements DepartmentService {
     };
   }
 
-  //定义一个ThreadLocal
-  private static ThreadLocal<Integer> threadLocal = new ThreadLocal<Integer>();
-
-  public static void main(String[] args) {
-    for(int i = 0; i < 2; i ++) {
-      new Thread(() -> {
-        int data = new Random().nextInt();
-        System.out.println(Thread.currentThread().getName() + " has put a data: " + data);
-        threadLocal.set(data);//直接往threadLocal里面里面扔数据即可
-        new TestA().getData();
-        new TestB().getData();
-      }).start();
-    }
-  }
-
-  static class TestA {
-    public void getData() {
-      System.out.println("A get data from " + Thread.currentThread().getName() + ": " + threadLocal.get());//直接取，不用什么关键字，它直接从当前线程中取
-    }
-  }
-
-  static class TestB {
-    public void getData() {
-      System.out.println("B get data from " + Thread.currentThread().getName() + ": " + threadLocal.get());//直接取，不用什么关键字，它直接从当前线程中取
-    }
-  }
-
-
-
 }
