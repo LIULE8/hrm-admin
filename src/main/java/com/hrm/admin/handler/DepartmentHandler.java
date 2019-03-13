43147bcab4ae3d87ca070544f26392dd239386c0
LIULE8 <scau_lzc@163.com>
2019-03-13 19:36:41 +0800

    fix bug -- department handler and delete some pojo

--- src/main/java/com/hrm/admin/handler/DepartmentHandler.java
+++ src/main/java/com/hrm/admin/handler/DepartmentHandler.java
@@ -2,28 +2,25 @@ package com.hrm.admin.handler;
 
 import com.hrm.admin.entities.Department;
 import com.hrm.admin.services.DepartmentService;
-import org.checkerframework.checker.nullness.qual.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import static org.springframework.http.MediaType.APPLICATION_STREAM_JSON;
-
 /**
  * @author LIULE9
  * @create 11/03/2019
  */
-@Component
+@RestController
+@RequestMapping("department")
 @SuppressWarnings("all")
+//@Component
 public class DepartmentHandler {
   @Autowired private DepartmentService departmentService;
 
   @GetMapping("/{id}")
-  @NonNull
   public Mono<Department> getOne(@PathVariable("id") String departmentId) {
     return departmentService.getOne(departmentId);
   }
